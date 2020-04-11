8446ccfc38aa63420312a6cfba6f00c7d6203aa4
WaarIsPierre <pierremj12@gmail.com>
2020-04-12 01:04:46 +0200

    working

--- Angular/mod3_solution/assignment3-starter-code/app.js
+++ Angular/mod3_solution/assignment3-starter-code/app.js
@@ -4,43 +4,45 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: 'A',
+    templateUrl: 'foundItems.html',
+    scope: {
+      foundItems: '<',
+      onRemove: '&'
+    },
+    controller: NarrowItDownController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+
+  return ddo;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
-  var found = this;
-  found.searchTerm=""
+  var menu = this;
+  menu.searchTerm="";
 
-  found.search = function (searchTerm) {
+  menu.search = function (searchTerm) {
     var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
 
     promise
     .then(function (result) {
-      var foundItem = result.data.menu_items
-
-      for (var i = 0; i < foundItem.length; i++) {
-        var item = foundItem[i];
-        var desc = item['description'];
-        var searchTerm = found.searchTerm.toLowerCase();
-        console.log(searchTerm);
-        if (desc.toLowerCase().indexOf(searchTerm) !== -1) {
-          console.log(desc);
-        } else {
-          foundItem.splice(i,1);
-        }
-      }
-
-      console.log(foundItem.length);
-      found.menu = foundItem;
+        menu.found = result;
     })
     .catch(function (error) {
       console.log(error);
     })
   };
 
-  found.removeItem = function(itemIndex) {
-    MenuSearchService.removeItem(itemIndex);
+  menu.removeItem = function (itemIndex) {
+    menu.found.splice(itemIndex,1);
   }
 
 }
@@ -51,14 +53,21 @@ function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
   service.getMatchedMenuItems = function (searchTerm) {
+    // var searchTerm = "rice";
     return $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json"),
-      // params: {
-      //   category: searchTerm
-      // }
-
-    });
+    })
+    .then(function (result) {
+      var foundItem =[];
+      for(let i=0; i<result.data['menu_items'].length; i++) {
+        if (result.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 && searchTerm.length > 0) {
+          console.log(result.data['menu_items'][i]);
+          foundItem.push(result.data['menu_items'][i])
+        }
+      }
+      return foundItem;
+    })
 
   };
 
